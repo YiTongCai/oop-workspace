2328b174a6ac24360d9e4c67217255dbd0402a91
CaiYiTong <130911653+CaiYiTong@users.noreply.github.com>
2024-03-02 00:10:20 +1030

    amend1-4

--- main-1-4.cpp
+++ main-1-4.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-extern int array_sum(int [], int [], int);
+extern int sum_two_arrays(int [], int [], int);
 
 int main() {
     int array[5] = {1,2,3,4,5};
     int secondarray[5]={6,7,8,9,10};
-    std::cout << "The sum is: " << array_sum(array,secondarray,5) << std::endl;
+    std::cout << "The sum is: " << sum_two_arrays(array,secondarray,5) << std::endl;
     return 0;
 }
