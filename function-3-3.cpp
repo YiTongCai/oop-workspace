6f76bc9e2c3c6269481ce524c86c6a0de0f388bf
CaiYiTong <130911653+CaiYiTong@users.noreply.github.com>
2024-03-01 23:24:07 +1030

    1&2&3

---
+++ function-3-3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+double weighted_average(int array[], int n){
+    if (n < 1){
+        return 0;
+    }
+    double sum = 0;
+    for (int i=0; i < n; i++) {
+        double count = 0;
+        for (int j=0; j < n; j++) {
+            if (array[i] == array[j]){
+                count ++ ;
+            }
+        }
+    sum += array[i]*count/n;
+    }
+}
