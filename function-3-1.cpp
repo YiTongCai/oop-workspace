ca196123f4bbddf2003dad861e1f29350e9bf978
CaiYiTong <130911653+CaiYiTong@users.noreply.github.com>
2024-03-02 01:39:16 +1030

    amend 3-1

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -3,8 +3,9 @@ bool is_fanarray(int array[], int n){
     if (n < 1){
         return false;
     }
-    for (int i=0 ; i < (n/2); i++) {
-        if (array[i]!=array[n-i]){
+    int len = n/2;
+    for (int i = 0 ; i < len ; i++) {
+        if (array[i]!=array[len-i]){
             return false;
         }
     }
