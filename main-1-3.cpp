95ac18852a5e2329e97531de2dbd7dfa8bc520b3
CaiYiTong <130911653+CaiYiTong@users.noreply.github.com>
2024-03-01 23:41:12 +1030

    amend 1-3

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -4,6 +4,6 @@ extern int num_count(int[], int, int);
 int main() {
     int array[10] = {1,2,3,4,5,2,3,4,5,6};
     int number = 2;
-    std::cout << "The number count is: " << num_count(array,10,2) << std::endl;
+    std::cout << "The number count is: " << num_count(array,10,number) << std::endl;
     return 0;
 }
