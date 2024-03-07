7348c34158809ae71bb25edf1e75f56216d6d77a
CaiYiTong <130911653+CaiYiTong@users.noreply.github.com>
2024-03-08 01:50:41 +1030

    amend

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -4,7 +4,7 @@
 int binary_to_int(int binary_digits[],int number_of_digits){
     int sum = 0;
     int power = 0;
-    for (int i = number_of_digits - 1 ; i < number_of_digits; i--){
+    for (int i = number_of_digits - 1 ; i >= 0; i--){
         if (binary_digits[i] == 1){
             sum += pow(2,power);
         }
