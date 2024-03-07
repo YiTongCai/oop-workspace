177f57a16530d831d3335ef24cb4b42445a5c3cf
CaiYiTong <130911653+CaiYiTong@users.noreply.github.com>
2024-03-08 01:37:07 +1030

    amend

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -3,12 +3,10 @@
 #include <algorithm>
 int binary_to_int(int binary_digits[],int number_of_digits){
     int sum = 0;
-    int power = 0;
-    for (int i =number_of_digits-1 ; 0< i < number_of_digits; i--){
-        if (binary_digits[i] == '1'){
-            sum += pow(2,power);
+    int count = 0;
+    for (int i = number_of_digits - 1 ; i < number_of_digits; i--){
+        sum += binary_digits[i] * 2^count;
         }
-        power++;
-    }
+        count++;
     return sum;
 }
