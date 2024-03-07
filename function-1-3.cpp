5884d8018d6c19f675706c680e543e644d4451e2
CaiYiTong <130911653+CaiYiTong@users.noreply.github.com>
2024-03-08 01:06:46 +1030

    amend 1-3

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -2,12 +2,12 @@
 void count_digits(int array[4][4]){
     int count[10] = {0};
      for (int i = 0; i < 4; i++){
-        for (int j = 0; j < 4; j++){
-            int n = array[i][j];
-            count[n]++;
+        for (int j = 0; j < 4; j++){;
+            count[array[i][j]]++;
         }  
     }
     for (int m = 0; m < 10; ++m){
         std::cout << m << ":" << count[m] <<std::endl;
-    }    
+    } 
+    std::cout<<std::endl;   
 }
