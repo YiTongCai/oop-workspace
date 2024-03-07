9e39ee63d72b15c6122a6507c77886f6a9183585
CaiYiTong <130911653+CaiYiTong@users.noreply.github.com>
2024-03-08 01:15:36 +1030

    amend

--- main-2-3.cpp
+++ main-2-3.cpp
@@ -4,10 +4,10 @@ extern int sum_array_elements(int integers[], int);
 extern int sum_if_palindrome(int integers[], int);
 
 int main() {
-    int integers[10]={1,2,3,4,5,5,4,3,2,1};
+    int test[]={1,2,3,4,5,5,4,3,2,1};
     int length = 10;
-    std::cout<<"the answer is :"<<is_palindrome(integers,length)<<std::endl;
-    std::cout<<"the answer is :"<<sum_array_elements(integers,length)<<std::endl;
-    std::cout<<"the answer is :"<<sum_if_palindrome(integers,length)<<std::endl;
+    std::cout<<"the answer is :"<<is_palindrome(test,length)<<std::endl;
+    std::cout<<"the answer is :"<<sum_array_elements(test,length)<<std::endl;
+    std::cout<<"the answer is :"<<sum_if_palindrome(test,length)<<std::endl;
     return 0;
 }
