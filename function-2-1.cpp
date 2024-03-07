98b748264d41bfb64bd3c6b45d75b69c6f67406b
CaiYiTong <130911653+CaiYiTong@users.noreply.github.com>
2024-03-08 01:46:40 +1030

    amend

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -6,26 +6,15 @@ void print_binary_str(std::string decimal_number){
     int number = std::stoi(decimal_number);
     std::string arr = "";
 
-    if (number < 1){
-        std::cout << "0" << std::endl;
+    if (number == 0){
+        arr = "0";
     }
-     
-    while (number >= 1) {
-        if (number > 1){
-            if (number % 2 != 0){
-                arr += "1";
-            }
-            if (number %2 == 0){
-                arr += "0";
-            }
-        }
-        if (number == 1){
-            arr += "0";
-            arr += "1";
-            number = number - 1;
-        }
 
+    while (number > 0) {
+        arr += std::to_string(number%2);
+        number = number / 2;
     }
+    
     std::reverse(arr.begin(),arr.end());
     std::cout<<arr<<std::endl;
 }
--- function-2-2.cpp
+++ function-2-2.cpp
@@ -3,10 +3,12 @@
 #include <algorithm>
 int binary_to_int(int binary_digits[],int number_of_digits){
     int sum = 0;
-    int count = 0;
+    int power = 0;
     for (int i = number_of_digits - 1 ; i < number_of_digits; i--){
-        sum += binary_digits[i] * 2^count;
+        if (binary_digits[i] == 1){
+            sum += pow(2,power);
         }
-        count++;
+        power++;
+    }
     return sum;
 }
