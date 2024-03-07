57291d30ffdbccd6921ffa0a3a131281a92b8f68
CaiYiTong <130911653+CaiYiTong@users.noreply.github.com>
2024-03-08 02:09:23 +1030

    amend

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 bool is_palindrome(int integers[], int length){
-    if (length < 1){
+    if (length <= 0){
         return false;
     }
     for (int i = 0 ; i < length/2 ; i++) {
         if (integers[i] != integers[length-i-1]){
             return false;
         }
+    }
     return true;
 } 
 
 int sum_array_elements(int integers[], int length){
     if (length < 1){
-        return false;
+        return -1;
     }
+    
     int sum = 0;
     for (int i = 0 ; i < length ; i++){
         sum += integers[i];
@@ -23,13 +25,12 @@ int sum_array_elements(int integers[], int length){
 
 int sum_if_palindrome(int integers[], int length){
     if (length < 1){
-        return false;
+        return -1;
     }
     if (!is_palindrome(integers,length)){
         return -2;
     }
     return sum_array_elements(integers,length);
 } 
-}
 
 
