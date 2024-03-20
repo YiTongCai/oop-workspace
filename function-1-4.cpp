3acf0c8625b75012befaa09dbef31dd9c50f89ec
CaiYiTong <130911653+CaiYiTong@users.noreply.github.com>
2024-03-21 01:19:07 +1030

    amend

--- function-1-4.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "Person.h"
-extern PersonList shallowCopyPersonList(PersonList pl){
-    PersonList List;
-    List.numPeople = pl.numPeople;
-    List.people = pl.people;
-    return pl;
-}
