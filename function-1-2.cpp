7f9bcd7b448fc7ebf5908ed0a91a0a108f8502de
CaiYiTong <130911653+CaiYiTong@users.noreply.github.com>
2024-03-21 01:27:26 +1030

    amend

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -5,7 +5,7 @@ PersonList createPersonList (int n){
     List.numPeople = n;
     List.people = new Person[n];
     for (int i = 0; i < n ;i++){
-        List.people[i].name = "John Doe";
+        List.people[i].name = "Jane Doe";
         List.people[i].age = 1;
     }
     return List;
